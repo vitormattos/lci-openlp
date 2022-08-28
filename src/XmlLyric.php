d067d1cf5da86b044f627b736f06bbd2cc8ea64b
Vitor Mattos <vitor@php.rio>
2022-08-27 23:59:32 -0300

    Remove unecessary methods

--- src/XmlLyric.php
+++ src/XmlLyric.php
@@ -11,48 +11,6 @@ class XmlLyric
         $this->lyric = $lyric;
     }
 
-    private function getTitles(): array
-    {
-        $titles[] = [
-            '__custom:title:1' => $this->lyric->getTitle(),
-        ];
-        if ($this->lyric->getTitleAlternative()) {
-            $titles[] = [
-                '__custom:title:2' => $this->lyric->getTitleAlternative(),
-            ];
-        }
-        return $titles;
-    }
-
-    private function getAuthors(): array
-    {
-        $return = [];
-        $sequence = 1;
-        foreach ($this->lyric->getAuthors() as $type => $authors) {
-            foreach ($authors as $author) {
-                $return['__custom:author:' . $sequence] = [
-                    '_attributes' => ['type' => $type],
-                    '_value' => $author,
-                ];
-                $sequence++;
-            }
-        }
-        return $return;
-    }
-
-    private function getThemes(): array
-    {
-        $topics = [];
-        $sequence = 1;
-        foreach ($this->lyric->getTopic() as $topic) {
-            $topics['__custom:theme:' . $sequence] = [
-                '_value' => $topic,
-            ];
-            $sequence++;
-        }
-        return $topics;
-    }
-
     public function getSongbooks(): array
     {
         $songbooks = [];
@@ -83,7 +41,6 @@ class XmlLyric
                         'type' => 'v',
                         'label' => $key,
                     ],
-                    // '_cdata' => str_replace( "\n", '<br />', $part ),
                     '_cdata' => $part,
                 ],
             ];
@@ -94,28 +51,14 @@ class XmlLyric
     public function __toString()
     {
         $lyric = [
-            // 'properties' => [
-            //     'titles' => $this->getTitles(),
-            // ],
             'lyrics' => $this->getVerses(),
         ];
-        // if ($this->lyric->countAuthors()) {
-        //     $lyric['properties']['authors'] = $this->getAuthors();
-        // }
-        // $lyric['properties']['songbooks'] = $this->getSongbooks();
-        // if ($this->lyric->getTopic()) {
-        //     $lyric['properties']['themes'] = $this->getThemes();
-        // }
         return ArrayToXml::convert(
             $lyric,
             [
                 'rootElementName' => 'song',
                 '_attributes' => [
-                    // 'xmlns' => 'http://openlyrics.info/namespace/2009/song',
                     'version' => '0.8',
-                    // 'createdIn' => 'OpenLP 2.9.5',
-                    // 'modifiedIn' => 'OpenLP 2.9.5',
-                    // 'modifiedDate' => (new \DateTime())->format('Y-m-d\TH:i:s'),
                 ],
             ],
             true,
